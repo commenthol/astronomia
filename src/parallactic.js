b488ff7b267ad5ba233310fdb4fca73eaf100e9c
commenthol <commenthol@gmail.com>
2016-06-19 12:54:07 +0200

    parallactic

---
+++ src/parallactic.js
@@ -0,0 +1,93 @@
+/**
+ * @copyright 2013 Sonia Keys
+ * @copyright 2016 commenthol
+ * @license MIT
+ */
+/**
+ * Parallactic: Chapter 14, The Parallactic Angle, and three other Topics.
+ */
+
+const base = require('./base')
+
+const M = exports
+
+/**
+ * ParallacticAngle returns parallactic angle of a celestial object.
+ *
+ *  φ is geographic latitude of observer.
+ *  δ is declination of observed object.
+ *  H is hour angle of observed object.
+ *
+ * All angles including result are in radians.
+ */
+M.parallacticAngle = function (φ, δ, H) { // (φ, δ, H float64)  float64
+  let [sδ, cδ] = base.sincos(δ)
+  let [sH, cH] = base.sincos(H)
+  return Math.atan2(sH, Math.tan(φ) * cδ - sδ * cH) // (14.1) p. 98
+}
+
+/**
+ * ParallacticAngleOnHorizon is a special case of ParallacticAngle.
+ *
+ * The hour angle is not needed as an input and the math inside simplifies.
+ */
+M.parallacticAngleOnHorizon = function (φ, δ) { // (φ, δ float64)  float64
+  return Math.acos(Math.sin(φ) / Math.cos(δ))
+}
+
+/**
+ * EclipticAtHorizon computes how the plane of the ecliptic intersects
+ * the horizon at a given local sidereal time as observed from a given
+ * geographic latitude.
+ *
+ *  ε is obliquity of the ecliptic.
+ *  φ is geographic latitude of observer.
+ *  θ is local sidereal time expressed as an hour angle.
+ *
+ *  λ1 and λ2 are ecliptic longitudes where the ecliptic intersects the horizon.
+ *  I is the angle at which the ecliptic intersects the horizon.
+ *
+ * All angles, arguments and results, are in radians.
+ */
+M.eclipticAtHorizon = function (ε, φ, θ) { // (ε, φ, θ float64)  (λ1, λ2, I float64)
+  let [sε, cε] = base.sincos(ε)
+  let [sφ, cφ] = base.sincos(φ)
+  let [sθ, cθ] = base.sincos(θ)
+  let λ = Math.atan2(-cθ, sε * (sφ / cφ) + cε * sθ) // (14.2) p. 99
+  if (λ < 0) {
+    λ += Math.PI
+  }
+  return [λ, λ + Math.PI, Math.acos(cε * sφ - sε * cφ * sθ)] // (14.3) p. 99
+}
+
+/**
+ * EclipticAtEquator computes the angle between the ecliptic and the parallels
+ * of ecliptic latitude at a given ecliptic longitude.
+ *
+ * (The function name EclipticAtEquator is for consistency with the Meeus text,
+ * and works if you consider the equator a nominal parallel of latitude.)
+ *
+ *  λ is ecliptic longitude.
+ *  ε is obliquity of the ecliptic.
+ *
+ * All angles in radians.
+ */
+M.eclipticAtEquator = function (λ, ε) { // (λ, ε float64)  float64
+  return Math.atan(-Math.cos(λ) * Math.tan(ε))
+}
+
+/**
+ * DiurnalPathAtHorizon computes the angle of the path a celestial object
+ * relative to the horizon at the time of its rising or setting.
+ *
+ *  δ is declination of the object.
+ *  φ is geographic latitude of observer.
+ *
+ * All angles in radians.
+ */
+M.diurnalPathAtHorizon = function (δ, φ) { // (δ, φ float64)  (J float64)
+  let tφ = Math.tan(φ)
+  let b = Math.tan(δ) * tφ
+  let c = Math.sqrt(1 - b * b)
+  return Math.atan(c * Math.cos(δ) / tφ)
+}
---
+++ test/parallactic.test.js
@@ -0,0 +1,31 @@
+/* global describe, it */
+
+var assert = require('assert')
+
+var parallactic = require('..').parallactic
+var sexa = require('..').sexagesimal
+
+describe('#parallactic', function () {
+  it('eclipticAtHorizon', function () {
+    var ε = 23.44 * Math.PI / 180
+    var φ = 51 * Math.PI / 180
+    var θ = 75 * Math.PI / 180
+    var res = parallactic.eclipticAtHorizon(ε, φ, θ)
+    var λ1 = res[0], λ2 = res[1], I = res[2] // eslint-disable-line one-var
+
+    assert.equal(new sexa.Angle(λ1).toString(0), '169°21′30″')
+    assert.equal(new sexa.Angle(λ2).toString(0), '349°21′30″')
+    assert.equal(new sexa.Angle(I).toString(0), '61°53′14″')
+  })
+
+  it('diurnalPathAtHorizon', function () {
+    var φ = 40 * Math.PI / 180
+    var ε = 23.44 * Math.PI / 180
+    var J = parallactic.diurnalPathAtHorizon(0, φ) // 0.8726646259971648
+    var Jexp = Math.PI / 2 - φ
+    assert.ok(Math.abs((J - Jexp) / Jexp) < 1e-15, new sexa.Angle(J).toString())
+    J = parallactic.diurnalPathAtHorizon(ε, φ) // 0.794553542331993
+    Jexp = new sexa.Angle(false, 45, 31, 0).rad()
+    assert.ok(Math.abs((J - Jexp) / Jexp) < 1e-3, new sexa.Angle(J).toString())
+  })
+})
