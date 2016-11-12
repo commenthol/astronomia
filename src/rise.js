e6e8eb2a4e5e4802c0387105b278fba3b762b6bf
commenthol <commenthol@gmail.com>
2016-11-12 23:41:08 +0100

    fixing rise function

--- src/rise.js
+++ src/rise.js
@@ -11,6 +11,9 @@
 const base = require('./base')
 const interp = require('./interpolation')
 const sexa = require('./sexagesimal')
+const sidereal = require('./sidereal')
+const deltat = require('./deltat')
+const julian = require('./julian')
 
 const M = exports
 
@@ -40,6 +43,105 @@ M.Stdh0Lunar = function (π) { // (π float64)  float64
  */
 const errorCircumpolar = new Error('Circumpolar')
 
+const SECS_PER_DEGREE = 240 // = 86400 / 360
+const SECS_PER_DAY = 86400
+const D2R = Math.PI / 180
+
+class Rise {
+  /**
+   * @param {number} lat - geographic latitude in degrees
+   * @param {number} lon - geographic longitude in degrees (measured positively westward)
+   * @param {number} jd - Julian Day
+   * @param {number} h0 - "standard altitude" of the body in radians
+   * @param {number|Array<number>} α - right ascension of the body.
+   * @param {number|Array<number>} δ - declination of the body.
+   */
+  constructor (lat, lon, jd, h0, α, δ) {
+    this.jd = jd
+    lat *= D2R // convert to radians
+    lon *= D2R
+    let Th0 = sidereal.apparent0UT(jd)
+    if (this._isArr3(α) && this._isArr3(δ)) {
+      let cal = new julian.Calendar().fromJD(jd)
+      let ΔT = deltat.deltaT(cal.toYear())
+      this._val = M.times({lat, lon}, ΔT, h0, Th0, α, δ)
+    } else {
+      this._val = M.approxTimes({lat, lon}, h0, Th0, α, δ)
+    }
+  }
+  /**
+   * @return {number} rise of body in julian days
+   */
+  rise () {
+    return this._toJD(this._val[0])
+  }
+  /**
+   * @return {number} transit of body in julian days
+   */
+  transit () {
+    return this._toJD(this._val[1])
+  }
+  /**
+   * @return {number} set of body in julian days
+   */
+  set () {
+    return this._toJD(this._val[2])
+  }
+  /**
+   * @private
+   */
+  _isArr3 (a) {
+    return Array.isArray(a) && a.length === 3
+  }
+  /**
+   * @private
+   */
+  _toJD (secs) {
+    return this.jd + secs / 86400
+  }
+}
+M.Rise = Rise
+
+/**
+ * @return {number} local angle in seconds per day `[0, 86400[`
+ */
+function _H (lat, h0, δ) {
+  // approximate local hour angle
+  let [sLat, cLat] = base.sincos(lat)
+  let [sδ1, cδ1] = base.sincos(δ)
+  let cosH = (Math.sin(h0) - sLat * sδ1) / (cLat * cδ1) // (15.1) p. 102
+  if (cosH < -1 || cosH > 1) {
+    throw errorCircumpolar
+  }
+  let H = Math.acos(cosH) * SECS_PER_DEGREE * 180 / Math.PI // in degrees per day === seconds
+  return H
+}
+
+/**
+ * @param {number} lon - longitude in radians
+ * @param {number} α - right ascension in radians
+ * @param {number} th0 - sidereal.apparent0UT in seconds of day `[0...86400[`
+ * @return {number} time of transit in seconds of day `[0, 86400[`
+ */
+function _mt (lon, α, th0) {
+  // let mt = (((lon + α) * 180 / Math.PI - (th0 * 360 / 86400)) * 86400 / 360)
+  let mt = (lon + α) * SECS_PER_DEGREE * 180 / Math.PI - th0
+  return mt
+}
+
+/**
+ * @param {number} Th0 - sidereal.apparent0UT in seconds of day `[0...86400[`
+ * @param {number} m - motion in seconds of day `[0...86400[`
+ * @return {number} new siderial time seconds of day `[0...86400[`
+ */
+function _th0 (Th0, m) {
+  // in original formula Th0 = 0...360 and m = 0...1 -> return value would be in 0...360 degrees
+  // Th0 /= 240
+  // m /= 86400
+  let th0 = base.pmod(Th0 + m * 360.985647 / 360, SECS_PER_DAY) // p103
+  return th0 // 0...86400 in seconds angle
+}
+
 /**
  * ApproxTimes computes approximate UT rise, transit and set times for
  * a celestial object on a day of interest.
@@ -63,25 +165,14 @@ const errorCircumpolar = new Error('Circumpolar')
  * Result units are seconds and are in the range [0,86400)
  * @throws Error
  */
-M.approxTimes = function (p, h0, Th0, α, δ) { // (p globe.Coord, h0, Th0 float64, α, δ float64)  (mRise, mTransit, mSet float64, err error)
-  // Meeus works in a crazy mix of units.
-  // This function and Times work with seconds of time as much as possible.
-
-  // approximate local hour angle
-  let [sLat, cLat] = base.sincos(p.lat)
-  let [sδ1, cδ1] = base.sincos(δ)
-  let cH0 = (Math.sin(h0) - sLat * sδ1) / (cLat * cδ1) // (15.1) p. 102
-  if (cH0 < -1 || cH0 > 1) {
-    throw errorCircumpolar
-  }
-  let H0 = Math.acos(cH0) * 43200 / Math.PI
-
+M.approxTimes = function (p, h0, Th0, α, δ) {
+  let H0 = _H(p.lat, h0, δ)
   // approximate transit, rise, set times.
   // (15.2) p. 102.0
-  let mt = (α + p.lon) * 43200 / Math.PI - Th0
-  let mTransit = base.pmod(mt, 86400)
-  let mRise = base.pmod(mt - H0, 86400)
-  let mSet = base.pmod(mt + H0, 86400)
+  let mt = _mt(p.lon, α, Th0)
+  let mTransit = base.pmod(mt, SECS_PER_DAY)
+  let mRise = base.pmod(mt - H0, SECS_PER_DAY)
+  let mSet = base.pmod(mt + H0, SECS_PER_DAY)
   return [mRise, mTransit, mSet]
 }
 
@@ -92,50 +183,50 @@ M.approxTimes = function (p, h0, Th0, α, δ) { // (p globe.Coord, h0, Th0 float
  * The function argurments do not actually include the day, but do include
  * a number of values computed from the day.
  *
- *  p is geographic coordinates of observer.
- *  ΔT is delta T.
- *  h0 is "standard altitude" of the body.
- *  Th0 is apparent sidereal time at 0h UT at Greenwich.
- *  α3, δ3 are slices of three right ascensions and declinations.
+ * @param {coord.Globe} p - is geographic coordinates of observer.
+ * @param {number} ΔT - is delta T in seconds
+ * @param {number} h0 - is "standard altitude" of the body in radians
+ * @param {number} Th0 - is apparent sidereal time at 0h UT at Greenwich in seconds
+ *        (range 0...86400) must be the time on the day of interest, in seconds.
+ *        See sidereal.apparent0UT
+ * @param {Array<number>} α3 - slices of three right ascensions
+ * @param {Array<number>} δ3 - slices of three declinations.
+ *        α3, δ3 must be values at 0h dynamical time for the day before, the day of,
+ *        and the day after the day of interest.  Units are radians.
  *
- * ΔT unit is seconds.  See package deltat.
- *
- * h0 unit is radians.
- *
- * Th0 must be the time on the day of interest, in seconds.
- * See sidereal.apparent0UT.
- *
- * α3, δ3 must be values at 0h dynamical time for the day before, the day of,
- * and the day after the day of interest.  Units are radians.
- *
- * Result units are seconds and are in the range [0,86400)
+ * @return Result units are seconds and are in the range [0,86400)
  * @throws Error
  */
 M.times = function (p, ΔT, h0, Th0, α3, δ3) { // (p globe.Coord, ΔT, h0, Th0 float64, α3, δ3 []float64)  (mRise, mTransit, mSet float64, err error)
   let [mRise, mTransit, mSet] = M.approxTimes(p, h0, Th0, α3[1], δ3[1])
-  let d3α = new interp.Len3(-86400, 86400, α3)
-  let d3δ = new interp.Len3(-86400, 86400, δ3)
+  let d3α = new interp.Len3(-SECS_PER_DAY, SECS_PER_DAY, α3)
+  let d3δ = new interp.Len3(-SECS_PER_DAY, SECS_PER_DAY, δ3)
 
   // adjust mTransit
-  let th0 = base.pmod(Th0 + mTransit * 360.985647 / 360, 86400)
-  let α = d3α.interpolateX(mTransit + ΔT)
-  let H = th0 - (p.lon + α) * 43200 / Math.PI
+  let ut = mTransit + ΔT
+  let α = d3α.interpolateX(ut)
+  let th0 = _th0(Th0, mTransit)
+  let H = -1 * _mt(p.lon, α, th0) // in secs // Hmeus = 0...360
   mTransit -= H
 
   // adjust mRise, mSet
   let [sLat, cLat] = base.sincos(p.lat)
 
-  let adjustRS = function (m) { // (float64, error) {
-    let th0 = base.pmod(Th0 + m * 360.985647 / 360, 86400)
+  let adjustRS = function (m) {
     let ut = m + ΔT
     let α = d3α.interpolateX(ut)
     let δ = d3δ.interpolateX(ut)
-    let H = th0 - (p.lon + α) * 43200 / Math.PI
+    let th0 = _th0(Th0, m)
+    let H = -1 * _mt(p.lon, α, th0)
+    let Hrad = (H / SECS_PER_DEGREE) * D2R
     let [sδ, cδ] = base.sincos(δ)
-    let h = sLat * sδ + cLat * cδ * Math.cos(H)
-    return m + (h - h0) / cδ * cLat * Math.sin(H)
+    let h = Math.asin(((sLat * sδ) + (cLat * cδ * Math.cos(Hrad)))) // formula 13.6
+    let Δm = (SECS_PER_DAY * (h - h0) / (cδ * cLat * Math.sin(Hrad) * 2 * Math.PI)) // formula p103 3
+    return m + Δm
   }
+
   mRise = adjustRS(mRise)
   mSet = adjustRS(mSet)
+
   return [mRise, mTransit, mSet]
 }
--- test/rise.test.js
+++ test/rise.test.js
@@ -13,56 +13,77 @@ var sidereal = require('..').sidereal
 var sexa = require('..').sexagesimal
 
 describe('#rise', function () {
-  it('approxTimes', function () {
-    // Example 15.a, p. 103.0
-    var jd = julian.CalendarGregorianToJD(1988, 3, 20)
-    var p = new globe.Coord(
-      new sexa.Angle(false, 42, 20, 0).rad(), // lat
-      new sexa.Angle(false, 71, 5, 0).rad()   // lon
-    )
-    // Meeus gives us the value of 11h 50m 58.1s but we have a package
-    // function for this:
-    var Th0 = sidereal.apparent0UT(jd)
-    var α = new sexa.RA(2, 46, 55.51).rad()
-    var δ = new sexa.Angle(false, 18, 26, 27.3).rad()
-    var h0 = rise.Stdh0Stellar
-    var a = rise.approxTimes(p, h0, Th0, α, δ)
-    var mrise = a[0], mtransit = a[1], mset = a[2]
-    // Units for approximate values given near top of p. 104 are circles.
-    assert.equal((mrise / 86400).toFixed(5), 0.51816)
-    assert.equal((mtransit / 86400).toFixed(5), 0.81965)
-    assert.equal((mset / 86400).toFixed(5), 0.12113)
+  var coord = {
+    lat: new sexa.Angle(false, 42, 20, 0),
+    lon: new sexa.Angle(false, 71, 5, 0)   // ! positively westward
+  }
+  var jd = julian.CalendarGregorianToJD(1988, 3, 20)
+  var h0 = rise.Stdh0Stellar
+
+  var α3 = [
+    new sexa.RA(2, 42, 43.25).rad(),
+    new sexa.RA(2, 46, 55.51).rad(),
+    new sexa.RA(2, 51, 7.69).rad()
+  ]
+  var δ3 = [
+    new sexa.Angle(false, 18, 2, 51.4).rad(),
+    new sexa.Angle(false, 18, 26, 27.3).rad(),
+    new sexa.Angle(false, 18, 49, 38.7).rad()
+  ]
+
+  var α = α3[1]
+  var δ = δ3[1]
+
+  describe('Rise', function () {
+    var lat = coord.lat.deg()
+    var lon = coord.lon.deg()
+
+    it('using approxTimes', function () {
+      // Example 15.a, p. 103.0
+      var rs = new rise.Rise(lat, lon, jd, h0, α, δ)
+      assert.equal(new julian.Calendar().fromJD(rs.rise()).toDate().toISOString(), '1988-03-20T12:26:09.176Z')
+      assert.equal(new julian.Calendar().fromJD(rs.transit()).toDate().toISOString(), '1988-03-20T19:40:17.417Z')
+      assert.equal(new julian.Calendar().fromJD(rs.set()).toDate().toISOString(), '1988-03-20T02:54:25.658Z')
+    })
+    it('using times', function () {
+      // Example 15.a, p. 103.0
+      var rs = new rise.Rise(lat, lon, jd, h0, α3, δ3)
+      assert.equal(new julian.Calendar().fromJD(rs.rise()).toDate().toISOString(), '1988-03-20T12:25:25.533Z')
+      assert.equal(new julian.Calendar().fromJD(rs.transit()).toDate().toISOString(), '1988-03-20T19:40:30.393Z')
+      assert.equal(new julian.Calendar().fromJD(rs.set()).toDate().toISOString(), '1988-03-20T02:54:39.932Z')
+    })
   })
 
-  it('times', function () {
-    // Example 15.a, p. 103.0
-    var cal = new julian.CalendarGregorian(1988, 3, 20)
+  describe('methods', function () {
     var p = new globe.Coord(
-      new sexa.Angle(false, 42, 20, 0).rad(), // lat
-      new sexa.Angle(false, 71, 5, 0).rad()   // lon
+      coord.lat.rad(),
+      coord.lon.rad()
     )
-    // Meeus gives us the value of 11h 50m 58.1s but we have a package
-    // function for this:
-    var Th0 = sidereal.apparent0UT(cal.toJD())
-    var α3 = [
-      new sexa.RA(2, 42, 43.25).rad(),
-      new sexa.RA(2, 46, 55.51).rad(),
-      new sexa.RA(2, 51, 7.69).rad()
-    ]
-    var δ3 = [
-      new sexa.Angle(false, 18, 2, 51.4).rad(),
-      new sexa.Angle(false, 18, 26, 27.3).rad(),
-      new sexa.Angle(false, 18, 49, 38.7).rad()
-    ]
-    var h0 = rise.Stdh0Stellar
-    // Similarly as with Th0, Meeus gives us the value of 56 for ΔT but
-    // let's use our package function.
-    var ΔT = deltat.deltaT(cal.toYear())
 
-    var a = rise.times(p, ΔT, h0, Th0, α3, δ3)
-    var mrise = a[0], mtransit = a[1], mset = a[2]
-    assert.equal(new sexa.Time(mrise).toString(0), '12ʰ26ᵐ9ˢ')
-    assert.equal(new sexa.Time(mtransit).toString(0), '19ʰ40ᵐ30ˢ')
-    assert.equal(new sexa.Time(mset).toString(0), '2ʰ54ᵐ26ˢ')
+    it('approxTimes', function () {
+      // Example 15.a, p. 103.0
+      // Meeus gives us the value of 11h 50m 58.1s but we have a package function for this:
+      var Th0 = sidereal.apparent0UT(jd)
+      var a = rise.approxTimes(p, h0, Th0, α, δ)
+      var mrise = a[0], mtransit = a[1], mset = a[2]
+      // Units for approximate values given near top of p. 104 are circles.
+      assert.equal((mrise / 86400).toFixed(5), 0.51816)
+      assert.equal((mtransit / 86400).toFixed(5), 0.81965)
+      assert.equal((mset / 86400).toFixed(5), 0.12113)
+    })
+
+    it('times', function () {
+      // Example 15.a, p. 103.0
+      // Meeus gives us the value of 11h 50m 58.1s but we have a package function for this:
+      var Th0 = sidereal.apparent0UT(jd)
+      // Similarly as with Th0, Meeus gives us the value of 56 for ΔT but
+      // let's use our package function.
+      var ΔT = deltat.deltaT(new julian.Calendar().fromJD(jd).toYear())
+      var a = rise.times(p, ΔT, h0, Th0, α3, δ3)
+      var mrise = a[0], mtransit = a[1], mset = a[2]
+      assert.equal(new sexa.Time(mrise).toString(0), '12ʰ25ᵐ26ˢ')
+      assert.equal(new sexa.Time(mtransit).toString(0), '19ʰ40ᵐ30ˢ')
+      assert.equal(new sexa.Time(mset).toString(0), '2ʰ54ᵐ40ˢ')
+    })
   })
 })
