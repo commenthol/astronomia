commenthol/date-chinese

break: esm module

src/index.js
@@ -3,7 +3,7 @@
  * @license MIT
  */
 
-export { default as CalendarChinese } from './Chinese'
-export { default as CalendarKorean } from './Korean'
-export { default as CalendarVietnamese } from './Vietnamese'
-export { default as CalendarJapanese } from './Japanese'
+export { default as CalendarChinese } from './Chinese.js'
+export { default as CalendarKorean } from './Korean.js'
+export { default as CalendarVietnamese } from './Vietnamese.js'
+export { default as CalendarJapanese } from './Japanese.js'

commenthol/astronomia

src/index.js
@@ -1,58 +1,58 @@
-export { default as angle } from './angle'
-export { default as apparent } from './apparent'
-export { default as apsis } from './apsis'
-export { default as base } from './base'
-export { default as binary } from './binary'
-export { default as coord } from './coord'
-export { default as conjunction } from './conjunction'
-export { default as circle } from './circle'
-export { default as data } from '../data'
-export { default as deltat } from './deltat'
-export { default as elementequinox } from './elementequinox'
-export { default as elliptic } from './elliptic'
-export { default as eclipse } from './eclipse'
-export { default as elp } from './elp'
-export { default as eqtime } from './eqtime'
-export { default as fit } from './fit'
-export { default as globe } from './globe'
-export { default as illum } from './illum'
-export { default as interpolation } from './interpolation'
-export { default as iterate } from './iterate'
-export { default as jm } from './jm'
-export { default as julian } from './julian'
-export { default as jupiter } from './jupiter'
-export { default as jupitermoons } from './jupitermoons'
-export { default as kepler } from './kepler'
-export { default as line } from './line'
-export { default as nearparabolic } from './nearparabolic'
-export { default as node } from './node'
-export { default as nutation } from './nutation'
-export { default as mars } from './mars'
-export { default as moon } from './moon'
-export { default as moonillum } from './moonillum'
-export { default as moonmaxdec } from './moonmaxdec'
-export { default as moonnode } from './moonnode'
-export { default as moonphase } from './moonphase'
-export { default as moonposition } from './moonposition'
-export { default as parabolic } from './parabolic'
-export { default as parallax } from './parallax'
-export { default as parallactic } from './parallactic'
-export { default as perihelion } from './perihelion'
-export { default as planetary } from './planetary'
-export { default as planetposition } from './planetposition'
-export { default as planetelements } from './planetelements'
-export { default as pluto } from './pluto'
-export { default as precess } from './precess'
-export { default as refraction } from './refraction'
-export { default as rise } from './rise'
-export { default as saturnmoons } from './saturnmoons'
-export { default as saturnring } from './saturnring'
-export { default as sexagesimal } from './sexagesimal'
-export { default as sidereal } from './sidereal'
-export { default as solar } from './solar'
-export { default as solardisk } from './solardisk'
-export { default as solarxyz } from './solarxyz'
-export { default as solstice } from './solstice'
-export { default as stellar } from './stellar'
-export { default as sundial } from './sundial'
-export { default as sunrise } from './sunrise'
+export { default as angle } from './angle.js'
+export { default as apparent } from './apparent.js'
+export { default as apsis } from './apsis.js'
+export { default as base } from './base.js'
+export { default as binary } from './binary.js'
+export { default as coord } from './coord.js'
+export { default as conjunction } from './conjunction.js'
+export { default as circle } from './circle.js'
+export { default as data } from '../data/index.js'
+export { default as deltat } from './deltat.js'
+export { default as elementequinox } from './elementequinox.js'
+export { default as elliptic } from './elliptic.js'
+export { default as eclipse } from './eclipse.js'
+export { default as elp } from './elp.js'
+export { default as eqtime } from './eqtime.js'
+export { default as fit } from './fit.js'
+export { default as globe } from './globe.js'
+export { default as illum } from './illum.js'
+export { default as interpolation } from './interpolation.js'
+export { default as iterate } from './iterate.js'
+export { default as jm } from './jm.js'
+export { default as julian } from './julian.js'
+export { default as jupiter } from './jupiter.js'
+export { default as jupitermoons } from './jupitermoons.js'
+export { default as kepler } from './kepler.js'
+export { default as line } from './line.js'
+export { default as nearparabolic } from './nearparabolic.js'
+export { default as node } from './node.js'
+export { default as nutation } from './nutation.js'
+export { default as mars } from './mars.js'
+export { default as moon } from './moon.js'
+export { default as moonillum } from './moonillum.js'
+export { default as moonmaxdec } from './moonmaxdec.js'
+export { default as moonnode } from './moonnode.js'
+export { default as moonphase } from './moonphase.js'
+export { default as moonposition } from './moonposition.js'
+export { default as parabolic } from './parabolic.js'
+export { default as parallax } from './parallax.js'
+export { default as parallactic } from './parallactic.js'
+export { default as perihelion } from './perihelion.js'
+export { default as planetary } from './planetary.js'
+export { default as planetposition } from './planetposition.js'
+export { default as planetelements } from './planetelements.js'
+export { default as pluto } from './pluto.js'
+export { default as precess } from './precess.js'
+export { default as refraction } from './refraction.js'
+export { default as rise } from './rise.js'
+export { default as saturnmoons } from './saturnmoons.js'
+export { default as saturnring } from './saturnring.js'
+export { default as sexagesimal } from './sexagesimal.js'
+export { default as sidereal } from './sidereal.js'
+export { default as solar } from './solar.js'
+export { default as solardisk } from './solardisk.js'
+export { default as solarxyz } from './solarxyz.js'
+export { default as solstice } from './solstice.js'
+export { default as stellar } from './stellar.js'
+export { default as sundial } from './sundial.js'
+export { default as sunrise } from './sunrise.js'